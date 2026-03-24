c042cc3caeabea211b48ba5666638e5a89dae7a5
julananp <julananp@gmail.com>
2026-03-24 13:29:06 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // ให้ /index.html ใน public/ เป็นหน้าหลัก
-  async rewrites() {
-    return [
-      {
-        source: '/',
-        destination: '/index.html',
-      },
-    ];
-  },
-};
-
+const nextConfig = {};
 module.exports = nextConfig;
+```
